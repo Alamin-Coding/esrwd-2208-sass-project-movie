1e06e1ad01812b7938234f324d8e7f3543a66003
Alamin-Coding <alamin.codelover@gmail.com>
2022-12-28 13:41:24 +0600

    navbar and banner complite

---
+++ js/script.js
@@ -0,0 +1,12 @@
+$(function(){
+    // "use strict";
+    $(".movie_slider").slick({
+        infinite: true,
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        arrows:false,
+        centerMode:true,
+        centerPadding: "45px",
+        focusOnSelect: true
+    })
+})
